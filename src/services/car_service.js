3760c6024b15f1bed01633649f859a7afa522d26
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-04-13 07:13:00 +0300

    hm

--- src/services/car_service.js
+++ src/services/car_service.js
@@ -4,7 +4,7 @@ import {urls} from "../constants";
 const user_service = {
     getAll: () => axios_service.get(urls.cars),
     getByid: (id) => axios_service(`${urls.cars}/${id}`),
-    adNewcar: () => axios_service.post(urls.cars),
+    adNewcar: (car) => axios_service.post(urls.cars, car),
     updNewcar: (id) => axios_service.put(`${urls.cars}/${id}`),
     delNewcar: (id) => axios_service.delete(`${urls.cars}/${id}`)
 }
