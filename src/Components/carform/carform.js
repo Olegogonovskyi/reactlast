9b2cd7b29b8220998f3f4c207bf4e5d0dbad4b99
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-04-13 16:28:58 +0300

    hm3 in proces

--- src/Components/carform/carform.js
+++ src/Components/carform/carform.js
@@ -5,26 +5,27 @@ import {car_service} from "../../services"
 
 
 const Carform = ({setnewcar}) => {
-    const {register, handleSubmit, reset} = useForm()
-    const {errortxt, seterrortxt} = useState();
+    const {register, handleSubmit, reset} = useForm();
+    // const {errortxt, seterrortxt} = useState({});
+
     const submit = async (car) => {
         try {
             const {data} = await car_service.adNewcar(car);
             setnewcar(data)
             reset()
-        }catch (e) {
-            seterrortxt(e.response.data)
+        } catch (e) {
+            // seterrortxt(e.response.data)
         }
 
     }
     return (
         <form onSubmit={handleSubmit(submit)}>
-            <div><label>Model<input type="text" {...register('model)}/></label></div>
-            {errortxt.model && <span>{errortxt.model[0]}</span>}
+            <div><label>Model<input type="text" {...register('model')}/></label></div>
+            {/*{errortxt.model && <span>{errortxt.model[0]}</span>}*/}
             <div><label>Price<input type="text" {...register('price', {valueAsNumber:true})}/></label></div>
-            {errortxt.price && <span>{errortxt.price[0]}</span>}
+            {/*{errortxt.price && <span>{errortxt.price[0]}</span>}*/}
             <div><label>Year<input type="text" {...register('year', {valueAsNumber:true})}/></label></div>
-            {errortxt.year && <span>{errortxt.year[0]}</span>}
+            {/*{errortxt.year && <span>{errortxt.year[0]}</span>}*/}
             <button>Save</button>
         </form>
     );
