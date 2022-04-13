533c23cef12834b4f157eeff253b95663c876798
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-04-13 17:02:19 +0300

    hm3 in proces

--- src/Components/carform/carform.js
+++ src/Components/carform/carform.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 
 import {useForm} from "react-hook-form";
 import {car_service} from "../../services"
+import {joiResolver} from "@hookform/resolvers/joi"
+import {carVal} from '../../validators'
 
 
 const Carform = ({setnewcar}) => {
-    const {register, handleSubmit, reset} = useForm();
+    const {register, handleSubmit, reset, formState:{errors}} = useForm({resolver:joiResolver(carVal)});
     // const {errortxt, seterrortxt} = useState({});
 
     const submit = async (car) => {
@@ -21,10 +23,13 @@ const Carform = ({setnewcar}) => {
     return (
         <form onSubmit={handleSubmit(submit)}>
             <div><label>Model<input type="text" {...register('model')}/></label></div>
+            {errors.model && <span>{errors.model.message}</span>}
             {/*{errortxt.model && <span>{errortxt.model[0]}</span>}*/}
             <div><label>Price<input type="text" {...register('price', {valueAsNumber:true})}/></label></div>
+            {errors.price && <span>{errors.price.message}</span>}
             {/*{errortxt.price && <span>{errortxt.price[0]}</span>}*/}
             <div><label>Year<input type="text" {...register('year', {valueAsNumber:true})}/></label></div>
+            {errors.year && <span>{errors.year.message}</span>}
             {/*{errortxt.year && <span>{errortxt.year[0]}</span>}*/}
             <button>Save</button>
         </form>
---
+++ src/validators/carValidator.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+const carVal = Joi.object({
+    model: Joi.string().min(1).max(20).require().messages({
+        'string.empty': 'Це поле не може бути пустим',
+        'string.min': 'Це поле не може бути пустим',
+        'string.max': 'Це поле не може бути пустим',
+
+    }),
+    price: Joi.number().min(1).max(10000000).require().messages({
+        'number.empty': 'Це поле не може бути пустим',
+        'number.min': 'Це поле не може бути пустим',
+        'number.max': 'Це поле не може бути пустим',
+
+    }),
+    year: Joi.number().min(1910).max(Date().getFullYear()).required()
+
+    })
+
+export {carVal}
