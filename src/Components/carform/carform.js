6ba8638e439e3cea9d368797f33045fd24d56440
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-04-13 16:04:03 +0300

    hm3 in proces

--- src/Components/carform/carform.js
+++ src/Components/carform/carform.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {useForm} from "react-hook-form";
 import {car_service} from "../../services"
@@ -6,17 +6,25 @@ import {car_service} from "../../services"
 
 const Carform = ({setnewcar}) => {
     const {register, handleSubmit, reset} = useForm()
+    const {errortxt, seterrortxt} = useState();
     const submit = async (car) => {
-        const {data} = await car_service.adNewcar(car);
-        setnewcar(data)
-    reset()
+        try {
+            const {data} = await car_service.adNewcar(car);
+            setnewcar(data)
+            reset()
+        }catch (e) {
+            seterrortxt(e.response.data)
+        }
 
     }
     return (
         <form onSubmit={handleSubmit(submit)}>
-            <div><label>Model<input type="text" {...register('model')}/></label></div>
+            <div><label>Model<input type="text" {...register('model)}/></label></div>
+            {errortxt.model && <span>{errortxt.model[0]}</span>}
             <div><label>Price<input type="text" {...register('price', {valueAsNumber:true})}/></label></div>
+            {errortxt.price && <span>{errortxt.price[0]}</span>}
             <div><label>Year<input type="text" {...register('year', {valueAsNumber:true})}/></label></div>
+            {errortxt.year && <span>{errortxt.year[0]}</span>}
             <button>Save</button>
         </form>
     );
